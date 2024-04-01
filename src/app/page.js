9605881a641ae1ba3775e80f3bf6d80d61594cc8
hope-k <hopekumordzie@gmail.com>
2024-04-01 15:24:06 +0000

    metadata

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Layout from "@/components/Layout/Layout";
 
 export const metadata = {
   title: "CargoFex",
-  description: "CargoFex is a logistics company that provides shipping, freight, and transport services.",
+  description: "Air, Land, and Sea Freight Services.",
   keywords: "cargo, shipping, logistics, freight, transport, delivery, trucking, supply chain, warehouse, distribution, air freight, sea freight, road freight, cargo fex, cargo fex, cargofex",
 };
 

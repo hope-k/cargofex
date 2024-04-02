a9666db781f0466a53a6fca256b436da5cafffbd
hope-k <hopekumordzie@gmail.com>
2024-04-02 03:35:00 +0000

    faviconv2

--- src/app/company/layout.js
+++ src/app/company/layout.js
@@ -1,7 +1,7 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 
-export default function CompanyLayou({ children }) {
+export default function CompanyLayout({ children }) {
   return (
     <>
       <Header />
